21c29eda2713d1e31263c0c98899cffacc212c38
pajith551@gmail.com <pajith551@gmail.com>
2021-12-30 03:09:20 +0530

    trailer added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { MovieList } from './MovieList';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import { Switch, Route, Link, Redirect } from "react-router-dom";
+import { Switch, Route, Link, Redirect, useParams } from "react-router-dom";
 
 function App() {
   const INITIAL_MOVIES = [
@@ -11,37 +11,43 @@ function App() {
       poster: "https://moviegalleri.net/wp-content/uploads/2020/01/Hero-Suriya-Soorarai-Pottru-Movie-Teaser-Release-Jan-7th-Poster-HD.jpg",
       name: "Soorarai Pottru",
       rating: "9.1",
-      summary: "Maara, a young man from a remote village, dreams of launching his own airline service. However, he must overcome several obstacles and challenges in order to be successful in his quest."
+      summary: "Maara, a young man from a remote village, dreams of launching his own airline service. However, he must overcome several obstacles and challenges in order to be successful in his quest.",
+      trailer: "https://www.youtube.com/embed/fa_DIwRsa9o"
     },
     {
       poster: "https://m.media-amazon.com/images/M/MV5BNDJhNWRjMjgtNzg1NS00YjBjLThlZjUtYTViNjdjOGZmNmQ2XkEyXkFqcGdeQXVyMjYwMDk5NjE@._V1_.jpg",
       name: "Baba",
       rating: "5.3",
-      summary: "Baba is an atheist who always questions the higher power. A turn of events bestows Baba with divine powers and he takes it as an opportunity to help the people in his state."
+      summary: "Baba is an atheist who always questions the higher power. A turn of events bestows Baba with divine powers and he takes it as an opportunity to help the people in his state.",
+      trailer: "https://www.youtube.com/embed/0U9uPZSOt8s"
     },
     {
       poster: "https://m.media-amazon.com/images/M/MV5BMjFkMTFjYjgtNDhkNS00MGNmLWJkZWMtZWIwNGYyOWE3MTM0XkEyXkFqcGdeQXVyMTE1MjcwOTA4._V1_FMjpg_UX1000_.jpg",
       name: "Maanaadu",
       rating: "9.3",
-      summary: "On the day of a public conference by the state's Chief Minister, his bodyguard and a police officer become stuck in a time loop."
+      summary: "On the day of a public conference by the state's Chief Minister, his bodyguard and a police officer become stuck in a time loop.",
+      trailer:"https://www.youtube.com/embed/t9retstFUlM"
     },
     {
       poster: "https://www.thehindu.com/entertainment/movies/z5lmd3/article35738194.ece/BINARY/jai",
       name: "Jai Bhim",
       rating: "9.5",
-      summary: "A pregnant woman from a primitive tribal community, searches desperately for her husband, who is missing from police custody. A High Court advocate rises in support to find her husband and seek justice for them."
+      summary: "A pregnant woman from a primitive tribal community, searches desperately for her husband, who is missing from police custody. A High Court advocate rises in support to find her husband and seek justice for them.",
+      trailer: "https://www.youtube.com/embed/Gc6dEDnL8JA"
     },
     {
       poster: "https://images-na.ssl-images-amazon.com/images/I/71JmxE8pC1L._RI_.jpg",
       name: "Annaamalai",
       rating: "7.7",
-      summary: "Annamalai, a milkman, and Ashok, the son of a rich businessman, are childhood friends. However Ashok's father disapproves of their friendship and attempts to create a rift between the two."
+      summary: "Annamalai, a milkman, and Ashok, the son of a rich businessman, are childhood friends. However Ashok's father disapproves of their friendship and attempts to create a rift between the two.",
+      trailer:"https://www.youtube.com/embed/9Qln87o5PvE"
     },
     {
       poster: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTWQ5phF7_mITj-vhcGLg5vGKSXF1KmhjWNblq-8PSdW8IAbICb",
       name: "Aayirathil Oruvan",
       rating: "8.1",
-      summary: "Anitha, a government official, embarks on a journey to find Chandramouli, an archaeologist, who went to Vietnam in order to search for any existence of the prince of the Chola dynasty."
+      summary: "Anitha, a government official, embarks on a journey to find Chandramouli, an archaeologist, who went to Vietnam in order to search for any existence of the prince of the Chola dynasty.",
+      trailer:"https://www.youtube.com/embed/n4OxFMlLB8I"
     },
     {
       poster: "https://static.moviecrow.com/gallery/20141008/46940-64ecbf36008c17a7bd5e0ccbe4daa47a.jpg",
@@ -104,7 +110,7 @@ function App() {
           <Redirect to="/movies" />
         </Route>
         <Route path="/movies/:id">
-          Movie Details
+          <MovieDetails movies={movies} />
         </Route>
         <Route path="/movies">
           <MovieList movies={movies} />
@@ -119,6 +125,35 @@ function App() {
         <Route path="**"><NotFound /></Route>
       </Switch>
 
+    </div>
+  );
+}
+
+function MovieDetails({ movies }) {
+  const { id } = useParams();
+  const movie = movies[id];
+  const styles = {
+    color: movie.rating < 8 ? "crimson" : "green",
+    fontWeight: "bold",
+  };
+
+  return (
+    <div className="movie-details-container">
+      <iframe width="100%" height="540" 
+      src= {movie.trailer}
+      title="YouTube video player" 
+      frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+      allowfullscreen></iframe>
+
+      <div className="movie-specs">
+
+        <h3 className="movie-name">
+          {movie.name} </h3>
+
+        <p className="movie-rating" style={styles}> ⭐ {movie.rating}</p>
+
+        <p className="movie-summary"> {movie.summary}</p>
+      </div>
     </div>
   );
 }
